88e8dd0048b660ac03a5d863c462bb69f64bff12
babbaj <babbaj45@gmail.com>
2018-12-30 18:48:32 -0500

    add mapcrafter and spigot paths to properties

--- src/main/java/com/matt/nocom/server/Logging.java
+++ src/main/java/com/matt/nocom/server/Logging.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public interface Logging {
-  Logger LOGGER = LogManager.getLogger();
+  Logger LOGGER = LogManager.getLogger("NoCom");
 
   static Logger getLogger() {
     return LOGGER;
--- src/main/java/com/matt/nocom/server/Properties.java
+++ src/main/java/com/matt/nocom/server/Properties.java
@@ -1,11 +1,20 @@
 package com.matt.nocom.server;
 
-import java.nio.file.Path;
+import java.io.File;
 import java.nio.file.Paths;
+import org.springframework.lang.Nullable;
 
 public interface Properties {
   int DISTANCE            = Integer.valueOf(System.getProperty("nocom.distance", "1000"));
   int VIEW_DISTANCE       = Integer.valueOf(System.getProperty("nocom.distance.view", String.valueOf(9 * 16)));
 
   String DATABASE_PATH    = System.getProperty("nocom.database", Paths.get("").resolve("locations.db").toAbsolutePath().toString());
+
+  String WORLDS_PATH      = System.getProperty("nocom.worlds", new File("worlds").getAbsolutePath());
+  String RENDER_PATH      = System.getProperty("nocom.renders", new File("renders").getAbsolutePath());
+
+  @Nullable
+  String MAPCRAFTER_PATH  = System.getProperty("nocom.mapcrafter"); // path to exe
+  @Nullable
+  String SPIGOT_PATH      = System.getProperty("nocom.spigot"); // path to jar
 }
