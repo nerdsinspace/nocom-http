e1600907262015a846f129dafa70e787494763f7
fr1kin <matt.fr1kin@gmail.com>
2019-01-01 23:31:46 -0600

    small performance changes

--- src/main/java/com/matt/nocom/server/util/factory/LocationGroupFactory.java
+++ src/main/java/com/matt/nocom/server/util/factory/LocationGroupFactory.java
@@ -4,7 +4,9 @@ import com.google.common.collect.Lists;
 import com.matt.nocom.server.model.Location;
 import com.matt.nocom.server.model.LocationGroup;
 import com.matt.nocom.server.model.Position;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,26 +29,26 @@ public class LocationGroupFactory {
       while (!locations.isEmpty()) {
         final LocationGroup head = locations.remove(0);
 
-        List<LocationGroup> neighbors = locations.stream()
-            .filter(head::isInSameWorld)
-            .filter(loc -> loc.isInGroup(head, rng))
-            .collect(Collectors.toList());
+        List<Position> positions = null;
+        Iterator<LocationGroup> it = locations.iterator();
+        while(it.hasNext()) {
+          LocationGroup loc = it.next();
+          if(head.isInSameWorld(loc) && head.isInGroup(loc, rng)) {
+            it.remove();
+            if(positions == null) positions = Lists.newArrayList();
+            positions.addAll(loc.getPositions());
+          }
+        }
 
-        if (!neighbors.isEmpty()) {
-          List<Position> merged = neighbors.stream()
-              .peek(locations::remove) // remove from locations list
-              .map(LocationGroup::getPositions)
-              .flatMap(List::stream)
-              .collect(Collectors.toList()); // merge all positions into head
-          merged.addAll(head.getPositions());
-          merged.sort(Comparator.comparingLong(Position::getTime));
+        if(positions != null && !positions.isEmpty()) {
+          positions.addAll(head.getPositions());
+          positions.sort(Comparator.comparingLong(Position::getTime));
 
-          head.setPositions(merged);
+          head.setPositions(positions);
           head.setup();
 
           changed = true;
         }
-
         improved.add(head);
       }
       locations.addAll(improved);
