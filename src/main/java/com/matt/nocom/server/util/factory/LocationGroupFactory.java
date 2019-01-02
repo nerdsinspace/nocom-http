a06999c0daefd0b85b8608ae27e041d73b0a908b
fr1kin <matt.fr1kin@gmail.com>
2019-01-01 22:57:24 -0600

    small changes to the grouping algorithm. still terribly slow and needs to be reworked entirely

--- src/main/java/com/matt/nocom/server/util/factory/LocationGroupFactory.java
+++ src/main/java/com/matt/nocom/server/util/factory/LocationGroupFactory.java
@@ -10,49 +10,47 @@ import java.util.stream.Collectors;
 
 public class LocationGroupFactory {
   public static List<LocationGroup> translate(List<Location> input, int range) {
+    final int rng = range*range;
+
     List<LocationGroup> locations = input.stream()
         .map(loc -> LocationGroup.builder()
             .server(loc.getServer())
             .dimension(loc.getDimension())
             .position(loc.getPosition())
-            .build().organize())
+            .build().setup())
         .collect(Collectors.toList());
 
-    if(range > 0) {
-      boolean changed;
-      do {
-        changed = false;
-        List<LocationGroup> improved = Lists.newArrayList();
-        while (!locations.isEmpty()) {
-          final LocationGroup head = locations.remove(0);
+    boolean changed;
+    do {
+      changed = false;
+      List<LocationGroup> improved = Lists.newArrayList();
+      while (!locations.isEmpty()) {
+        final LocationGroup head = locations.remove(0);
 
-          List<LocationGroup> neighbors = locations.stream()
-              .filter(head::isInSameWorld)
-              .filter(loc -> loc.isInGroup(head, range))
-              .collect(Collectors.toList());
+        List<LocationGroup> neighbors = locations.stream()
+            .filter(head::isInSameWorld)
+            .filter(loc -> loc.isInGroup(head, rng))
+            .collect(Collectors.toList());
 
-          if (!neighbors.isEmpty()) {
-            List<Position> merged = neighbors.stream()
-                .peek(locations::remove) // remove from locations list
-                .map(LocationGroup::getPositions)
-                .flatMap(List::stream)
-                .collect(Collectors.toList()); // merge all positions into head
-            merged.addAll(head.getPositions());
-            merged.sort(Comparator.comparingLong(Position::getTime));
+        if (!neighbors.isEmpty()) {
+          List<Position> merged = neighbors.stream()
+              .peek(locations::remove) // remove from locations list
+              .map(LocationGroup::getPositions)
+              .flatMap(List::stream)
+              .collect(Collectors.toList()); // merge all positions into head
+          merged.addAll(head.getPositions());
+          merged.sort(Comparator.comparingLong(Position::getTime));
 
-            improved.add(LocationGroup.builder()
-                .server(head.getServer())
-                .dimension(head.getDimension())
-                .positions(merged)
-                .build().organize());
+          head.setPositions(merged);
+          head.setup();
 
-            changed = true;
-          } else
-            improved.add(head);
+          changed = true;
         }
-        locations.addAll(improved);
-      } while (changed);
-    }
+
+        improved.add(head);
+      }
+      locations.addAll(improved);
+    } while (changed);
 
     return locations;
   }
