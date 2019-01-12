d27b56f25a912d7248dabd412a165c8ba09ea607
fr1kin <matt.fr1kin@gmail.com>
2019-01-11 20:51:11 -0600

    use a custom model for user registration

---
+++ src/main/java/com/matt/nocom/server/model/auth/UserRegistration.java
@@ -0,0 +1,23 @@
+package com.matt.nocom.server.model.auth;
+
+import com.matt.nocom.server.auth.UserGroup;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Singular;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistration {
+  private String username;
+  private String password;
+
+  @Singular
+  private Set<UserGroup> groups;
+}
