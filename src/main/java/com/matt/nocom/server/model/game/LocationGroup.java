196e69ddeb0e0cb8c40f3158ecebd97aa1582f5c
babbaj <babbaj45@gmail.com>
2019-01-14 22:56:28 -0500

    fix import

--- src/main/java/com/matt/nocom/server/model/game/LocationGroup.java
+++ src/main/java/com/matt/nocom/server/model/game/LocationGroup.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.matt.nocom.server.model.LocationGroup.Serializer;
+import com.matt.nocom.server.model.game.LocationGroup.Serializer;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
