74a9c72bde2cd73ea03b356a1bd11e2438b7ef0c
fr1kin <matt.fr1kin@gmail.com>
2019-08-11 19:52:46 -0500

    spacing fix

--- src/main/java/com/matt/nocom/server/Properties.java
+++ src/main/java/com/matt/nocom/server/Properties.java
@@ -10,23 +10,23 @@ public interface Properties {
   /**
    * SQL Dialect to use for jOOQ
    */
-  SQLDialect SQL_DIALECT  = SQLDialect.SQLITE;
+  SQLDialect SQL_DIALECT = SQLDialect.SQLITE;
   
   /**
    * Enable to apply properties tokenizer to IDE builds
    */
-  boolean DEV_MODE        = Boolean.parseBoolean(System.getProperty("nocom.dev", "false"));
+  boolean DEV_MODE = Boolean.parseBoolean(System.getProperty("nocom.dev", "false"));
   
   /**
    * Authentication will use username:password provided by DEBUG_USERNAME/DEBUG_PASSWORD
    */
-  boolean DEBUG_AUTH      = Boolean.parseBoolean(System.getProperty("nocom.auth.debug", "false"));
+  boolean DEBUG_AUTH = Boolean.parseBoolean(System.getProperty("nocom.auth.debug", "false"));
   
   /**
    * Debug mode username and password
    */
-  String DEBUG_USERNAME   = System.getProperty("nocom.auth.debug.username", "root");
-  String DEBUG_PASSWORD   = System.getProperty("nocom.auth.debug.password", "pass");
+  String DEBUG_USERNAME = System.getProperty("nocom.auth.debug.username", "root");
+  String DEBUG_PASSWORD = System.getProperty("nocom.auth.debug.password", "pass");
   
   /**
    * A file that specifies a list of user=pass combinations
@@ -36,26 +36,26 @@ public interface Properties {
   /**
    * Time it takes for an access token to expire
    */
-  long TOKEN_EXPIRATION   = Long.parseLong(System.getProperty("nocom.login.expiration", String.valueOf(Duration.ofDays(1).toMillis()))); // default to 1 day
+  long TOKEN_EXPIRATION = Long.parseLong(System.getProperty("nocom.login.expiration", String.valueOf(Duration.ofDays(1).toMillis()))); // default to 1 day
   
   /**
    * Minimum length of a password
    */
-  int MIN_PASSWORD_LEN    = Integer.parseInt(System.getProperty("nocom.min.pass.length", "8"));
+  int MIN_PASSWORD_LEN = Integer.parseInt(System.getProperty("nocom.min.pass.length", "8"));
   
   /**
    * World directory
    */
-  String WORLDS_PATH      = System.getProperty("nocom.worlds", Paths.get("worlds").toAbsolutePath().toString());
+  String WORLDS_PATH = System.getProperty("nocom.worlds", Paths.get("worlds").toAbsolutePath().toString());
   
   /**
    * ? unused
    */
   @Nullable
-  String SPIGOT_PATH      = System.getProperty("nocom.spigot"); // path to jar
+  String SPIGOT_PATH = System.getProperty("nocom.spigot"); // path to jar
   
   /**
    * List of HTTP headers that may expose the clients real IP behind a reverse proxy
    */
-  String[] HTTP_REAL_IP_HEADERS   = System.getProperty("nocom.http.ip.headers", "X-REAL-IP").split(",");
+  String[] HTTP_REAL_IP_HEADERS = System.getProperty("nocom.http.ip.headers", "X-REAL-IP").split(",");
 }
