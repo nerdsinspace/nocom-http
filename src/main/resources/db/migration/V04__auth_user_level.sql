6faf68da91e2ecfee29869a2f3ced07ee83f168b
fr1kin <matt.fr1kin@gmail.com>
2019-09-12 19:18:51 -0500

    Fixed the horrible mess the code reformat made

--- src/main/resources/db/migration/V04__auth_user_level.sql
+++ src/main/resources/db/migration/V04__auth_user_level.sql
@@ -10,35 +10,29 @@ ALTER TABLE Auth_Users
     ADD last_login bigint DEFAULT 0;
 
 -- [jooq ignore start]
-DELETE
-FROM Auth_Users
+DELETE FROM Auth_Users
 WHERE id IN (
     SELECT user_id
-    FROM Auth_User_Groups
-             LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
+    FROM Auth_User_Groups LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
     WHERE (Auth_User_Groups.id = Auth_Users.id)
       AND (Auth_Groups.name = 'DEBUG')
 );
 
-UPDATE Auth_Users
-SET level = 90
+UPDATE Auth_Users SET level = 90
 WHERE EXISTS(
-              SELECT *
-              FROM Auth_User_Groups
-                       LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
-              WHERE (Auth_User_Groups.id = Auth_Users.id)
-                AND (Auth_Groups.name = 'ADMIN')
-          );
+    SELECT * FROM Auth_User_Groups
+        LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
+    WHERE (Auth_User_Groups.id = Auth_Users.id)
+      AND (Auth_Groups.name = 'ADMIN')
+);
 
-UPDATE Auth_Users
-SET level = 50
+UPDATE Auth_Users SET level = 50
 WHERE EXISTS(
-              SELECT *
-              FROM Auth_User_Groups
-                       LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
-              WHERE (Auth_User_Groups.id = Auth_Users.id)
-                AND (Auth_Groups.name = 'DEV')
-          );
+    SELECT * FROM Auth_User_Groups
+        LEFT JOIN Auth_Groups ON (Auth_User_Groups.group_id = Auth_Groups.id)
+    WHERE (Auth_User_Groups.id = Auth_Users.id)
+      AND (Auth_Groups.name = 'DEV')
+);
 -- [jooq ignore stop]
 
 DROP TABLE Auth_Groups;
