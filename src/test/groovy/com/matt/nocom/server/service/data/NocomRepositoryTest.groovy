ef315c7501de1f073e53c1089a2df4bcd99ec58e
mattcc <matt.fr1kin@gmail.com>
2020-06-30 18:32:37 -0500

    Fix unit test

--- src/test/groovy/com/matt/nocom/server/service/data/NocomRepositoryTest.groovy
+++ src/test/groovy/com/matt/nocom/server/service/data/NocomRepositoryTest.groovy
@@ -10,19 +10,25 @@ import spock.lang.Specification
 
 import java.time.Duration
 
+import static com.matt.nocom.server.postgres.codegen.Tables.PLAYERS
 import static com.matt.nocom.server.postgres.codegen.Tables.PLAYER_SESSIONS
 
 class NocomRepositoryTest extends Specification {
   def "GetPlayerSessions"() {
     MockDataProvider dataProvider = { MockExecuteContext exe ->
-      System.out.println exe.sql()
       def ctx = DSL.using(SQLDialect.POSTGRES);
-      def r0 = ctx.newResult(PLAYER_SESSIONS.JOIN);
-      def r1 = ctx.newResult(PLAYER_SESSIONS.LEAVE);
-      return [
-          new MockResult(0, r0),
-          new MockResult(0, r1),
-      ] as MockResult[];
+      if (exe.sql().startsWith("select \"players\".\"username\"")) {
+        def r0 = ctx.newResult(PLAYERS.USERNAME);
+        r0.add(ctx.newRecord(PLAYERS.USERNAME).values("testing"));
+        return [new MockResult(1, r0)] as MockResult[];
+      } else {
+        def r0 = ctx.newResult(PLAYER_SESSIONS.JOIN);
+        def r1 = ctx.newResult(PLAYER_SESSIONS.LEAVE);
+        return [
+            new MockResult(0, r0),
+            new MockResult(0, r1),
+        ] as MockResult[];
+      }
     }
 
     def connection = new MockConnection(dataProvider);
