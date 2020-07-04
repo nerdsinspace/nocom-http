a4d098d53448719fb8d958e973fb2e984c4cc3b3
mattcc <matt.fr1kin@gmail.com>
2020-07-03 22:27:35 -0500

    More code refactoring & bug fixes with authentication

--- src/main/java/com/matt/nocom/server/controller/LoginController.java
+++ src/main/java/com/matt/nocom/server/controller/LoginController.java
@@ -1,28 +1,22 @@
 package com.matt.nocom.server.controller;
 
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.matt.nocom.server.config.auth.JWTConfiguration;
-import com.matt.nocom.server.model.auth.AuthenticatedUser;
 import com.matt.nocom.server.model.auth.JwtToken;
 import com.matt.nocom.server.model.auth.User;
 import com.matt.nocom.server.properties.JWTProperties;
 import com.matt.nocom.server.service.EventRepository;
-import com.matt.nocom.server.service.auth.UserAuthenticationProvider;
 import com.matt.nocom.server.service.auth.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
 import java.time.Instant;
 
 import static com.matt.nocom.server.Logging.getLogger;
@@ -37,7 +31,7 @@ public class LoginController {
   private final UserRepository users;
 
   @PostMapping("/login")
-  public AuthenticatedUser login(@RequestParam String username, @RequestParam String password,
+  public String login(@RequestParam String username, @RequestParam String password,
       HttpServletResponse response) {
     var auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     var user = (User) auth.getPrincipal();
@@ -57,29 +51,24 @@ public class LoginController {
     events.publishUser(auth, "/login", "Logged in");
     users.setUserLastLogin(user.getUsername(), Instant.now());
 
-    return AuthenticatedUser.builder()
-        .username(user.getUsername())
-        .level(user.getLevel())
-        .token(token)
-        .build();
+    // token contains all the encoded details
+    return token;
   }
 
   @PostMapping("/validate")
-  public AuthenticatedUser validate(Authentication authentication) {
-    var user = (User) authentication.getPrincipal();
-    var token = (String) authentication.getCredentials();
-    return AuthenticatedUser.builder()
-        .username(user.getUsername())
-        .level(user.getLevel())
-        .token(token)
-        .build();
+  public ResponseEntity<Object> validate() {
+    // the user must have a valid session token in order to reach this method
+    // otherwise they will get an authentication error
+    return ResponseEntity.ok().build();
   }
 
   @ExceptionHandler(AuthenticationException.class)
   @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid credentials")
-  public void handleException(AuthenticationException e) { }
+  public void handleException(AuthenticationException e) {
+  }
 
   @ExceptionHandler({JWTVerificationException.class, UsernameNotFoundException.class})
   @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid token")
-  public void handleVerificationException(JWTVerificationException e) { }
+  public void handleVerificationException(JWTVerificationException e) {
+  }
 }
